05510eaf6829a8d956b71bb7e485649f4fb02ebb
rkmv8 <rickardkarlmartin@gmail.com>
2022-03-09 15:40:31 +0100

    Further darkmode adjustments and added repo links

--- script.js
+++ script.js
@@ -7,9 +7,20 @@ if (toggle.checked == true){
         element.style.backgroundColor = '#012A4A'
        });
 
-    document.querySelectorAll('p, h1, ul, li, h2, h3').forEach(element => {
-        element.style.color = 'white'
+       document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3').forEach(element => {
+        element.style.backgroundColor = '#012A4A'
        });
+
+    document.querySelectorAll('p, h1, ul, li, h2, h3').forEach(element => {
+        element.style.color = '#fffafa'
+       });
+
+       document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3, header, .projectbanner, .project2title, .project2image, .project2description, .project1image img, .project2image img, .project3image img, #bild').forEach(element => {
+        element.style.borderColor = 'grey'
+       });
+
+
+       
 }
 else{
 
@@ -17,9 +28,13 @@ else{
         element.style.backgroundColor = '#F1FAEE'
        });
 
-    Array.from(document.querySelectorAll('p, h1, ul, li, h2, h3')).forEach(element => {
+    Array.from(document.querySelectorAll('p, ul, li, h1, h2, h3')).forEach(element => {
         element.style.color = 'black'
        });
+
+       document.querySelectorAll('#dms').forEach(element => {
+        element.style.color = '#fffafa';
+    })
 }
 }
 
