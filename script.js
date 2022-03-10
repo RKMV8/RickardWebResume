ce75ba04e6671c5454dad4717ebb3f081b296f09
rkmv8 <rickardkarlmartin@gmail.com>
2022-03-10 21:48:54 +0100

    Made some changes to darkmode/lightmode

--- script.js
+++ script.js
@@ -4,38 +4,47 @@ var toggle = document.getElementById('check');
 
 if (toggle.checked == true){
     document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3').forEach(element => {
-        element.style.backgroundColor = '#012A4A'
-       });
+    element.style.backgroundColor = '#343A40'
+    });
 
-       document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3').forEach(element => {
-        element.style.backgroundColor = '#012A4A'
-       });
+    document.querySelectorAll('header, .projectbanner').forEach(element => {
+        element.style.backgroundColor = '#212529'
+    })
 
     document.querySelectorAll('p, h1, ul, li, h2, h3').forEach(element => {
         element.style.color = '#fffafa'
-       });
+        });
 
-       document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3, header, .projectbanner, .project2title, .project2image, .project2description, .project1image img, .project2image img, .project3image img, #bild').forEach(element => {
-        element.style.borderColor = 'grey'
-       });
-       document.getElementById('lightbuttontext').innerHTML = "Darkmode"
+    document.querySelectorAll('#dms').forEach(element => {
+        element.style.color = '#fffafa'
+    })
 
+    document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3, header, .projectbanner, .project2title, .project2image, .project2description, .project1image img, .project2image img, .project3image img, #bild').forEach(element => {
+    element.style.borderColor = 'grey'
+    });   
 
-       
+    
+    document.querySelector('#linkedinimg').src="./images/linkedinlight.svg";
+    document.querySelector('#githubimg').src="./images/githublight.svg"
+    console.log('test');
 }
 else{
-    document.getElementById('lightbuttontext').innerHTML = "Lightmode"
     document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3').forEach(element => {
-        element.style.backgroundColor = '#F1FAEE'
+        element.style.backgroundColor = '#e9f5db'
        });
 
     Array.from(document.querySelectorAll('p, ul, li, h1, h2, h3')).forEach(element => {
         element.style.color = 'black'
        });
 
-       document.querySelectorAll('#dms').forEach(element => {
-        element.style.color = '#fffafa';
+       document.querySelectorAll('header, .projectbanner').forEach(element => {
+        element.style.backgroundColor = '#cfe1b9'
     })
+
+    document.querySelector('#linkedinimg').src="./images/linkedindark.svg";
+    document.querySelector('#githubimg').src="./images/githubdark.svg"
+
+       
 }
 }
 
