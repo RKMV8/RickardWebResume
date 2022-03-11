dd7ebe7333e0b54b9ec9e7d94d6c7ff82ce82372
rkmv8 <rickardkarlmartin@gmail.com>
2022-03-11 09:20:26 +0100

    Further improved dark/light and overall css changes

--- script.js
+++ script.js
@@ -21,7 +21,9 @@ if (toggle.checked == true){
 
     document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3, header, .projectbanner, .project2title, .project2image, .project2description, .project1image img, .project2image img, .project3image img, #bild').forEach(element => {
     element.style.borderColor = 'grey'
-    });   
+    }); 
+    
+    document.querySelector('#lightbuttontext').innerHTML = "Dark";
 
     
     document.querySelector('#linkedinimg').src="./images/linkedinlight.svg";
@@ -33,15 +35,17 @@ if (toggle.checked == true){
 }
 else{
     document.querySelectorAll('.project1, .project2, .project3, .item1, .item2, .item3').forEach(element => {
-        element.style.backgroundColor = '#e9f5db'
+        element.style.backgroundColor = '#C9CBCC'
        });
+
+       document.querySelector('#lightbuttontext').innerHTML = "Light";
 
     Array.from(document.querySelectorAll('p, ul, li, h1, h2, h3')).forEach(element => {
         element.style.color = 'black'
        });
 
        document.querySelectorAll('header, .projectbanner').forEach(element => {
-        element.style.backgroundColor = '#cfe1b9'
+        element.style.backgroundColor = '#BBC0C4'
     })
 
     document.querySelector('#linkedinimg').src="./images/linkedindark.svg";
